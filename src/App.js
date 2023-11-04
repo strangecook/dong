9a5602acb2f9c663c1ec67a053418addd74dfc70
strangecook <skyheo123@gmail.com>
2023-11-04 23:25:19 +1000

    basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from "./Home";
+import Login from "./login";
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="navbar">
+          <Link to="/" className="nav-item">홈</Link>
+          <Link to="/login" className="nav-item">소개</Link>
+        </div>
+        <Routes>
+          <>
+            <Route path="/" element={<Home />}> </Route>
+            <Route path="/login" element={<Login />}> </Route>
+          </>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
